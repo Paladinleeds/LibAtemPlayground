bce4cfe13a5b616353c521bb2c9fc3ad4d10de68
Ellie <10800494+Paladinleeds@users.noreply.github.com>
2024-06-19 09:43:30 +0100

    Initial Commit - will be followed up with a licence commit

---
+++ LibAtemConnectAndProgramPreviewTransitions/Config.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using LibAtem.Common;
+
+namespace ShortcutsAtemKeyboard
+{
+	public class Config
+	{
+		public Dictionary<MixEffectBlockId, MixEffectConfig> MixEffect { get; set; }
+
+		public Dictionary<SuperSourceBoxId, Dictionary<char, VideoSource>> SuperSource { get; set; }
+
+		public class MixEffectConfig
+		{
+			public Dictionary<char, VideoSource> Program { get; set; }
+			public Dictionary<char, VideoSource> Preview { get; set; }
+
+			public char Cut { get; set; }
+			public char Auto { get; set; }
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# AtemSwitcherPlayground
+
+This repository will look at functionality and provide examples on how to make things work with [LibAtem](https://github.com/LibAtem/LibAtem) from basic console apps. You are free to look at the code used here to see how things function across the board, and consider how aspects of this code can be implemented into your own projects.
+
+## Licence
+
+For the purposes of compliance with LibAtem using the L-GPL, and to keep things open source, this project is licenced under the AGPLv3.
